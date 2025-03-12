267d2448b5ddf37834ac37e8ad0cf197c289ddac
Soumayabderahmen <soumaya.abderahmen@esprit.tn>
2025-03-12 11:18:31 +0100

    Ajout de la fonctionnalité d'authentification

---
+++ app/Models/Startup.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+class Startup extends Model
+{
+    use HasFactory;
+    protected $table = 'startup';
+    protected $fillable = ['user_id', 'logo_startup','phone_number'];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
